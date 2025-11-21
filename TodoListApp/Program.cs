d10762af07eda3b7fb5b29d98849e17006f4e56f
nguyenquocanh <nguyenquocanh170205@gmail.com>
2025-11-21 11:41:51 +0700

    Initial commit

---
+++ TodoListApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using TodoListApp.Services;
+
+var service = new TaskService();
+
+while (true)
+{
+    Console.WriteLine("\n=== TODO LIST ===");
+    Console.WriteLine("1. Add Task");
+    Console.WriteLine("2. Delete Task");
+    Console.WriteLine("3. Show Tasks");
+    Console.WriteLine("4. Exit");
+    Console.Write("Choose: ");
+
+    var choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            Console.Write("Enter task: ");
+            service.AddTask(Console.ReadLine());
+            break;
+
+        case "2":
+            Console.Write("Enter ID to delete: ");
+            service.DeleteTask(int.Parse(Console.ReadLine()));
+            break;
+
+        case "3":
+            var tasks = service.GetTasks();
+            Console.WriteLine("\nCurrent tasks:");
+            foreach (var t in tasks)
+                Console.WriteLine($"{t.Id}. {t.Title} | Done: {t.IsDone}");
+            break;
+
+        case "4":
+            return;
+
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+}
+
+//using System;
+//string filePath = "Data/task.txt";
+
+//if (File.Exists(filePath))
+//{
+//    File.Delete(filePath);
+//    Console.WriteLine("File đã được xóa.");
+//}
+//else
+//{
+//    Console.WriteLine("File không tồn tại.");
+//}
---
+++ TodoListApp/Services/TaskService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TodoListApp.Models;
+
+namespace TodoListApp.Services
+{
+    internal class TaskService
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+        private readonly string filePath = "Data/task.txt";
+
+        public TaskService() 
+        {
+            LoadFromFile();
+        }
+
+        public List<TaskItem> GetTasks() => tasks;
+        
+        public void AddTask(string title)
+        {
+            tasks.Add(new TaskItem { Id = tasks.Count, Title = title, IsDone = false });
+            SaveToFile();
+        }
+
+        public void DeleteTask(int id)
+        {
+            tasks.RemoveAll(x => x.Id == id);
+            SaveToFile();
+        }
+        public void SaveToFile()
+        {
+            Directory.CreateDirectory("Data");
+            File.WriteAllLines(filePath, tasks.Select(t => $"{t.Id} |{t.Title}|{t.IsDone}"));
+        }
+
+        private void LoadFromFile()
+        {
+            if (!File.Exists(filePath)) return;
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                tasks.Add(new TaskItem 
+                {
+                    Id = int.Parse(parts[0].Trim()),
+                    Title = parts[1].Trim(),
+                    IsDone = bool.Parse(parts[2].Trim())
+                });
+            }
+        }
+    }
+}
